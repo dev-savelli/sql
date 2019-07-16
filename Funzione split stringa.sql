2ae261bc09e3993ecefe2ff6a6635bc3375f3ccf
Devteam Savelli <52315731+dev-savelli@users.noreply.github.com>
2019-07-16 09:45:43 +0200

    Update Funzione split stringa.sql

--- Funzione split stringa.sql
+++ Funzione split stringa.sql
@@ -10,10 +10,10 @@ GO
 
 /* funzionae di tipo tabella che accetta come parametri una stringa e un delimitatore, restituisce una tabella
 con le colonne ID, Dati dove in dati ci sono i pezzi splittati dal delimitatore
-Es. SELECT * FROM dbo.split('primo_secondo_terzo' , '_') 
+Es. SELECT * FROM dbo.SAVFSplit('primo_secondo_terzo' , '_') 
 */
 
-ALTER FUNCTION [dbo].[Split]
+ALTER FUNCTION [dbo].[SAVFSplit]
 (
 @String NVARCHAR(4000),
 @Delimiter NCHAR(1)
@@ -22,17 +22,17 @@ RETURNS TABLE
 AS
 RETURN 
 (
-WITH Split(stpos,endpos) 
+WITH SAVFSplit(stpos,endpos) 
 AS(
 SELECT 0 AS stpos, CHARINDEX(@Delimiter,@String) AS endpos
 UNION ALL
 SELECT endpos+1, CHARINDEX(@Delimiter,@String,endpos+1)
-FROM Split
+FROM SAVFSplit
 WHERE endpos > 0
 )
 SELECT 'id' = ROW_NUMBER() OVER (ORDER BY (SELECT 1)),
 'dati' = SUBSTRING(@String,stpos,COALESCE(NULLIF(endpos,0),LEN(@String)+1)-stpos)
-FROM Split
+FROM SAVFSplit
 )
 GO
 
