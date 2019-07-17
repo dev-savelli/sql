f2e4603d419e786c69acf45bcbf038e43e52fd84
Devteam Savelli <52315731+dev-savelli@users.noreply.github.com>
2019-07-17 09:29:59 +0200

    Update emaildb_esempio1.sql

--- email utility/emaildb_esempio1.sql
+++ email utility/emaildb_esempio1.sql
@@ -1,34 +1,56 @@
+
 /* Invio risultato query direttamente da sql 
 si può usare il parametro @query per avere nel corpo direttamente il risultato in formato testo 
 si può usare il tag '<pre>' se si usa un formato corpo html per avere la spaziatura fissa del carattere per una migliore leggibilità
 
 MOLTO MEGLIO però usare una tabella html composta direttamente nel corpo!
+Si usa la select con la direttiva FOR XML PATH che converte l'output in xml i cui tag per riga possono essere sostituiti con il tag <tr> (riga tabella html)
 */
 
 DECLARE @corpo NVARCHAR(MAX);
-SET @corpo = '<H2> Elenco Clienti </H2>' + -- tag per titolo
-             '<table style="border-collapse:collapse;" border="1" cellpadding="3">' + -- tag di inizio tabella
-             '<tr> <th>Conto</th> <th>descrizione</th> <th>indirizzo</th> </tr>' + -- tag per riga header con i nomi delle colonne
-             
-			 --query che estrae i dati in formato xml mettendo per ogni riga il tag <tr> riga di tabella, e per ogni campo mette il tag <td> (la cella) e </td> (é il '' tra i campi)
-			 --l'istruzione FOR XML PATH capisce che il '' tra i campi é il </td> , il CAST serve per restituire un nvarchar che si unisce al resto della stringa 
+-- compone l'html del titolo, la tabella, la riga con l'header della tabella con il nome delle colonne
+SET @corpo = '<H2> Situazione magazzino </H2>  
+             <table style="border-collapse:collapse;" border="1" cellpadding="3">  
+             <tr>
+			 <th style="background-color:lavender;">Articolo</th>  
+			 <th style="background-color:lavender;">Descrizione</th>
+			 <th style="background-color:lavender;">UM</th>
+			 <th style="background-color:yellow;">Esistenza</th>
+			 <th style="background-color:lavender;">Ordinato</th>
+			 <th style="background-color:lavender;">Impegnato</th>
+			 </tr>' + 
+             -- unisce alla stringa il risultato della query che estrae i dati in formato xml 
+			 -- mettendo per ogni riga il tag <tr> riga di tabella, e per ogni campo mette il tag <td> (la cella) 
+             -- l'istruzione FOR XML PATH capisce che il '' tra i campi é il </td> , il CAST serve per assicurare un nvarchar che si unisce al resto della stringa 
              CAST(
 (
-    SELECT td = an_conto, 
+    SELECT td = artico.ar_codart, 
            '', 
-           td = an_descr1, 
+           td = artico.ar_descr, 
            '', 
-           td = an_indir, 
+           td = artico.ar_unmis, 
+           '', 
+           td = format(artprox.apx_esist, '#.##'), 
+           '', 
+           td = format(artprox.apx_ordin, '#.##'), 
+           '', 
+           td = format(artprox.apx_impeg, '#.##'), 
            ''
-    FROM anagra
-    WHERE an_tipo = 'C' FOR XML PATH('tr')
-) AS NVARCHAR(MAX)) + '</table>';
+    FROM artico
+         INNER JOIN artprox ON artico.ar_codart = artprox.apx_codart
+                               AND artico.codditt = artprox.codditt
+    WHERE apx_esist > 0
+          AND apx_ordin > 0
+    ORDER BY artico.ar_codart FOR XML PATH('tr')
+) AS NVARCHAR(MAX)) + '</table>' --chiude il tag della tabella
+
+--PRINT @corpo;
 
 --esegue la sp che invia l'email con la tabella generata direttamente nel corpo
 EXEC msdb.dbo.sp_send_dbmail 
      @profile_name = 'avvisi', 
      @execute_query_database = 'PROVA3', 
-     @recipients = 'simo@savell.om', 
+     @recipients = 'massi@savel.co', 
      @subject = 'invio email da sp', 
      @body_format = 'HTML', 
      @body = @corpo;
